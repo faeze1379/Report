ecddbc70b21ade9402d17fbd8b7359a8b8d49de6
faeze1379 <fazyfirozifar@gmail.com>
2021-02-22 04:15:53 -0800

    99/12/4

--- layout/saveReport/saveReportController.js
+++ layout/saveReport/saveReportController.js
@@ -1,11 +1,17 @@
 app.controller("saveReportController", function ($scope) {
   $scope.set = function () {
+    var reports = JSON.parse(localStorage.getItem('userReport'));
+
+    reports = !reports ? []:reports;
+
+
     var a = {
       name: $scope.myName,
       data: $scope.mydata,
       decrip: $scope.mytext,
     }
-    localStorage.setItem("userReport", JSON.stringify(a));
+    reports.push(a);
+    localStorage.setItem("userReport", JSON.stringify(reports));
     var x=JSON.parse(localStorage.getItem("userReport"));
     alert(" گزارش " + x.name +" ثبت شد ")
   };
--- layout/showReport/showReportController.js
+++ layout/showReport/showReportController.js
@@ -1,12 +1,4 @@
 app.controller("showReportController", function ($scope) {
-  var x=[]
-   x = JSON.parse(localStorage.getItem("userReport"));
+  $scope.reports = JSON.parse(localStorage.getItem("userReport"));
    
-  $scope.reports = [
-    {
-      data: x.name,
-      family: x.data,
-      report: x.decrip,
-    },
-  ];
 });
