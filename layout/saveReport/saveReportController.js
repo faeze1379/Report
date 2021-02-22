112ae69f7cdd3edad82c5b2855f52e3b19246284
faeze1379 <fazyfirozifar@gmail.com>
2021-02-22 03:17:10 -0800

    1399/12/04

--- layout/saveReport/saveReportController.js
+++ layout/saveReport/saveReportController.js
@@ -1,21 +1,13 @@
 app.controller("saveReportController", function ($scope) {
   $scope.set = function () {
-    var b=[]
     var a = {
       name: $scope.myName,
       data: $scope.mydata,
       decrip: $scope.mytext,
     }
     localStorage.setItem("userReport", JSON.stringify(a));
-    
-    
-  };
-  //   $scope.get=function()
-  //   {
-  //     var x=JSON.parse(localStorage.getItem('userReport'));
-  //   }
-  $scope.save = function () {
-    alert("saved..");
+    var x=JSON.parse(localStorage.getItem("userReport"));
+    alert(" گزارش " + x.name +" ثبت شد ")
   };
   $scope.left = function () {
     alert("Are you sure you want to cancel?..");
--- layout/showReport/showReportController.js
+++ layout/showReport/showReportController.js
@@ -1,42 +1,12 @@
-
-app.controller('showReportController',function ($scope)
-{
-  $scope.get=function()
-  {
-    JSON.parse(localStorage.getItem('userReport'));
-    
-  }
-    $scope.reports = [
-      {
-        data: "99/08/11",
-        family: "فائزه فیروزی فر",
-        report: "اشنایی با انگولار ",
-      },
-      {
-        data: "99/08/11",
-        family: "فائزه فیروزی فر",
-        report: "اشنایی با انگولار ",
-      },
-      {
-        data: "99/08/11",
-        family: "فائزه فیروزی فر",
-        report: "اشنایی با انگولار ",
-      },
-      {
-        data: "99/05/13",
-        family: "محبوبه علیزاده",
-        report: " اشنایی با یو ای کیت ها ",
-      },
-      {
-        data: "99/05/13",
-        family: "محبوبه علیزاده",
-        report: " اشنایی با یو ای کیت ها ",
-      },
-      {
-        data: "99/05/13",
-        family: "محبوبه علیزاده",
-        report: " اشنایی با یو ای کیت ها ",
-      },
-    ];
-  });
-  
+app.controller("showReportController", function ($scope) {
+  var x=[]
+   x = JSON.parse(localStorage.getItem("userReport"));
+   
+  $scope.reports = [
+    {
+      data: x.name,
+      family: x.data,
+      report: x.decrip,
+    },
+  ];
+});
