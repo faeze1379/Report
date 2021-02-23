b6851c3843412d3c0df6e9addd5db93f8085ccd5
faeze1379 <fazyfirozifar@gmail.com>
2021-02-23 09:32:06 -0800

    delete to listRgister

--- layout/listRegister/listRegisterController.js
+++ layout/listRegister/listRegisterController.js
@@ -1,3 +1,16 @@
 app.controller("listRegisterController", function ($scope) {
   $scope.users = JSON.parse(localStorage.getItem("users"));
+  $scope.delete=function(record){
+    var users=JSON.parse(localStorage.getItem("users"));
+    //console.log(users)
+    var index = users.findIndex(function(item) {
+      return item.name == record.name;
+    });
+    users.splice(index,1);
+
+    console.log(users);
+    localStorage.setItem("users",JSON.stringify(users));
+    $scope.users = JSON.parse(localStorage.getItem("users"));
+  }
+ 
 });
